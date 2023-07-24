2de482eccbdf1fba7c1f568a334d1ac1c169446e
BaBamBa <rushknight75@gmail.com>
2023-07-24 18:20:48 +0900

    jwt 추가

---
+++ java-spring/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO member(`id`,`email`) values (1, 'jpatest1@test.com');
+INSERT INTO member(`id`,`email`) values (2, 'jpatest2@test.com');
+INSERT INTO member(`id`,`email`) values (3, 'jpatest3@test.com');
---
+++ java-spring/src/main/resources/sqls/dml.sql
@@ -0,0 +1,3 @@
+INSERT INTO member(`id`,`email`) values (1, 'test1@test.com');
+INSERT INTO member(`id`,`email`) values (2, 'test2@test.com');
+INSERT INTO member(`id`,`email`) values (3, 'test3@test.com');
